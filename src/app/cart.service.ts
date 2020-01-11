beaaa262097676ea816b7c6d2eb07a9055827298
MiguelC98 <mikell998.mc@gmail.com>
2020-01-10 23:09:52 -0700

    2 files modified, 2 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -19,7 +19,11 @@ export class CartService {
     this.items = [];
     return this.items;
   }
-  
+
+  getShippingPrices() {
+    return this.http.get('/assets/shipping.json');
+  }
+
   constructor(
     private http: HttpClient
   ) { }
